a06cc45a26be0e1bd6218333a29be6c00d930fc9
Pratap Mridha <pratapmridha@gmail.com>
2023-05-10 12:24:50 +0530

    changes

--- browserTest/src/main.js
+++ browserTest/src/main.js
@@ -1,10 +1,10 @@
-const {EthereumEip712Signature2021}=require('ethereumeip712signature2021suite');
+import { EthereumEip712Signature2021 } from 'ethereumeip712signature2021suite'
 import { purposes } from 'jsonld-signatures'
 
 const Web3 = require("web3");
 const ethEnabled = async () => {
   if (window.ethereum) {
-    await window.ethereum.request({method: 'eth_requestAccounts'});
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
     window.web3 = new Web3(window.ethereum);
     return true;
   }
@@ -12,92 +12,92 @@ const ethEnabled = async () => {
 }
 
 const vector1 = {
-    "privateKey": "0x149195a4059ac8cafe2d56fc612f613b6b18b9265a73143c9f6d7cfbbed76b7e",
-    "inputDocument": {
-        "@context": ["https://schema.org", "https://w3id.org/security/v2"],
-        "@type": "Person",
-        "firstName": "Jane",
-        "lastName": "Does",
-        "jobTitle": "Professor",
-        "telephone": "(425) 123-4567",
-        "email": "jane.doe@example.com"
-    },
-    "inputOptions": {
-        "date": "2021-08-30T13:28:02Z",
-        "verificationMethod": "did:hid:testnet:0xAED7EA8035eEc47E657B34eF5D020c7005487443#key-1",
-        "domain": {
-            "name": "Test"
-        }
-    },
-    "proof": {
-        "created": "2021-08-30T13:28:02Z",
-        "proofPurpose": "assertionMethod",
-        "type": "EthereumEip712Signature2021",
-        "verificationMethod": "did:hid:testnet:0xAED7EA8035eEc47E657B34eF5D020c7005487443#key-1",
-        "proofValue": "0x3d4518532cb589760742045bce8deb57dab474a3db500a7162b53ed43b034ef332e7e7f0c361140c366505e820ee4146f4fdd918e3dd27286f70df6435ab0b821c",
-        "eip712Domain": {
-            "domain": {
-                "name": "Test"
-            },
-            "messageSchema": {
-                "Document": [
-                    {
-                        "name": "@context",
-                        "type": "string[]"
-                    },
-                    {
-                        "name": "@type",
-                        "type": "string"
-                    },
-                    {
-                        "name": "email",
-                        "type": "string"
-                    },
-                    {
-                        "name": "firstName",
-                        "type": "string"
-                    },
-                    {
-                        "name": "jobTitle",
-                        "type": "string"
-                    },
-                    {
-                        "name": "lastName",
-                        "type": "string"
-                    },
-                    {
-                        "name": "telephone",
-                        "type": "string"
-                    }
-                ]
-            },
-            "primaryType": "Document"
-        }
+  "privateKey": "0x149195a4059ac8cafe2d56fc612f613b6b18b9265a73143c9f6d7cfbbed76b7e",
+  "inputDocument": {
+    "@context": ["https://schema.org", "https://w3id.org/security/v2"],
+    "@type": "Person",
+    "firstName": "Jane",
+    "lastName": "Does",
+    "jobTitle": "Professor",
+    "telephone": "(425) 123-4567",
+    "email": "jane.doe@example.com",
+  },
+  "inputOptions": {
+    "date": "2021-08-30T13:28:02Z",
+    "verificationMethod": "did:hid:testnet:0xAED7EA8035eEc47E657B34eF5D020c7005487443#key-1",
+    "domain": {
+      "name": "Test"
     }
+  },
+  "proof": {
+    "created": "2021-08-30T13:28:02Z",
+    "proofPurpose": "assertionMethod",
+    "type": "EthereumEip712Signature2021",
+    "verificationMethod": "did:hid:testnet:0xAED7EA8035eEc47E657B34eF5D020c7005487443#key-1",
+    "proofValue": "0xd8ced27b921866a9cb6fb859503714ad4be03ae70706237d05c9f113da7f4a1d7f74f9f9df4301571f5c4f253c09e1b5119a85f00760f33924d72a07d31881ec1b",
+    "eip712Domain": {
+      "domain": {
+        "name": "Test"
+      },
+      "messageSchema": {
+        "Document": [
+          {
+            "name": "@context",
+            "type": "string[]"
+          },
+          {
+            "name": "@type",
+            "type": "string"
+          },
+          {
+            "name": "email",
+            "type": "string"
+          },
+          {
+            "name": "firstName",
+            "type": "string"
+          },
+          {
+            "name": "jobTitle",
+            "type": "string"
+          },
+          {
+            "name": "lastName",
+            "type": "string"
+          },
+          {
+            "name": "telephone",
+            "type": "string"
+          }
+        ]
+      },
+      "primaryType": "Document"
+    }
+  }
 };
 
 const customDocLoader = (url) => {
-    //@ts-ignore
-    const context = documents[url];
-  
-    if (context) {
-      return {
-        contextUrl: null, // this is for a context via a link header
-        document: context, // this is the actual document that was loaded
-        documentUrl: url, // this is the actual context URL after redirects
-      };
-    }
-  
-    throw new Error(
-      `Error attempted to load document remotely, please cache '${url}'`
-    );
-  };
+  //@ts-ignore
+  const context = documents[url];
+
+  if (context) {
+    return {
+      contextUrl: null, // this is for a context via a link header
+      document: context, // this is the actual document that was loaded
+      documentUrl: url, // this is the actual context URL after redirects
+    };
+  }
+
+  throw new Error(
+    `Error attempted to load document remotely, please cache '${url}'`
+  );
+};
 
 document.getElementById("initiate").addEventListener("click", async () => {
-   await  ethEnabled()
+  await ethEnabled()
 
-const ethereumeip712signature2021suite = new EthereumEip712Signature2021({},window.web3)
-const proof = await ethereumeip712signature2021suite.createProof({
+  const ethereumeip712signature2021suite = new EthereumEip712Signature2021({}, window.web3)
+  const proof = await ethereumeip712signature2021suite.createProof({
     document: vector1.inputDocument,
     purpose: new purposes.AssertionProofPurpose(),
     verificationMethod: vector1.inputOptions.verificationMethod,
@@ -107,7 +107,7 @@ const proof = await ethereumeip712signature2021suite.createProof({
 
 
   })
-document.getElementById('proof').innerHTML=JSON.stringify(proof,null,2)
+  document.getElementById('proof').innerHTML = JSON.stringify(proof, null, 2)
 
 
 
@@ -119,7 +119,7 @@ document.getElementById('proof').innerHTML=JSON.stringify(proof,null,2)
     purpose: new purposes.AssertionProofPurpose(),
     documentLoader: customDocLoader,
   });
-document.getElementById('verificationResult').innerHTML=JSON.stringify(verificationResult,null,2)
+  document.getElementById('verificationResult').innerHTML = JSON.stringify(verificationResult, null, 2)
 
 
 })
